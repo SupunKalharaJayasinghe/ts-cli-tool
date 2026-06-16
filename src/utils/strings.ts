b4952e3b882896cf4f20bf7740df1dc9ade89d6e
Supun Kalhara Jayasinghe <rmskjayasinghe2025@gmail.com>
2026-06-16 23:51:22 +0530

    feat: implement Website blueprint generator

---
+++ src/utils/strings.ts
@@ -0,0 +1,15 @@
+export function toPascal(value: string): string {
+  return value
+    .split(/[-_\s]+/)
+    .filter(Boolean)
+    .map((part) => `${part.charAt(0).toUpperCase()}${part.slice(1)}`)
+    .join('');
+}
+
+export function toTitle(value: string): string {
+  return value
+    .split(/[-_\s]+/)
+    .filter(Boolean)
+    .map((part) => `${part.charAt(0).toUpperCase()}${part.slice(1)}`)
+    .join(' ');
+}
